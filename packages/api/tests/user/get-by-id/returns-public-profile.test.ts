acacb6bbc9d538df998c3ae815385e3a1aa61032
Sobebar Ali <sobebar.ali17@gmail.com>
2025-11-25 17:43:05 +0530

    refactor(tests): remove console log from public profile retrieval test

--- packages/api/tests/user/get-by-id/returns-public-profile.test.ts
+++ packages/api/tests/user/get-by-id/returns-public-profile.test.ts
@@ -28,7 +28,5 @@ it("returns a public profile for getById", async () => {
 
 	const result = await caller.user.getById({ id: "other_user" });
 
-	console.log(result);
-
 	expect(result).toEqual({ user: publicProfile });
 });
